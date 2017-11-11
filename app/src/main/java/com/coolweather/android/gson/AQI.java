bf26e67de2c1be7d7758a78ce4edc9299ead2c1d
Shanks <qiyu9797@gmail.com>
2017-11-11 17:15:23 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by asus on 2017/11/11.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
